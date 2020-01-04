793307a127472dd3aa6a47b615038d48cc72ad2a
Nikko Solon <nikkosolon24@gmail.com>
2020-01-03 17:42:37 -0800

    Implemented solution to max pairwise product

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -3,30 +3,21 @@
 #include <algorithm>
 
 long long MaxPairwiseProduct(const std::vector<int>& numbers) {
-    int max_product = 0;
     int n = numbers.size();
-    int maxNum1 = 0;
-    int maxNum2 = 0;
-	if(n >= 2){
-        // find 2 largest numbers and multiply them
-        for(int i = 0; i < n; i++){
-            if(numbers[i] > maxNum1){
-                maxNum1 = numbers[i];    
-            }
+
+    int index1 = -1;
+    for (int i = 0; i < n; ++i){
+        if ((index1 == -1) || (numbers[i] > numbers[index1])){
+            index1 = i;
         }
-        for(int i = 0; i < n; i++){
-            if(numbers[i] > maxNum2){
-                if(numbers[i] == maxNum1){
-                    continue;
-                }
-                maxNum2 = numbers[i];
-            }
-        }
-	}else{
-        std::cout << "No Pairs" << std::endl;
-        return 1;
     }
-    return (long long)maxNum1 * maxNum2 ;
+    int index2 = -1;
+    for (int j = 0; j < n; ++j){
+        if ((j != index1) && ((index2 == -1) || (numbers[j] > numbers[index2]))){
+            index2 = j;
+        }
+    }
+    return ((long long) (numbers[index1])) * numbers[index2];
 }
 
 int main() {
