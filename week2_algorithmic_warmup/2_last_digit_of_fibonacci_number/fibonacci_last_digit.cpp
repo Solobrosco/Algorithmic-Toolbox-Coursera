605b6616da85c9dee914fe7002b585422166aa9d
Nikko Solon <nikkosolon24@gmail.com>
2020-01-09 19:26:30 -0800

    Implemented get_fibnoacci_digit_fast()

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -16,9 +16,22 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
+int get_fibonacci_last_digit_fast(long long n){
+    if(n <= 1){
+        return n;
+    }
+    int b = 0, a = 1;
+    for(int i = 0; i < n; ++i){
+        int temp = b % 10;
+        b = a % 10;
+        a = temp + a % 10; 
+    } 
+    return a % 10;
+}
+
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+    int c = get_fibonacci_last_digit_fast(n);
     std::cout << c << '\n';
     }
