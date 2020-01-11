e19c9b68666a5bfcc1d8d84e7cada2de1ea2413d
Nikko Solon <nikkosolon24@gmail.com>
2020-01-10 22:45:02 -0800

    Implemented fibonacci_sum_squares()

--- week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
+++ week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -18,8 +18,38 @@ int fibonacci_sum_squares_naive(long long n) {
     return sum % 10;
 }
 
+int get_fibonacci_last_digit_fast(long long n){
+    if(n <= 1){
+        return n;
+    }
+    int b = 0, a = 1;
+    for(int i = 0; i < n - 1; ++i){
+        int temp = b % 10;
+        b = a % 10;
+        a = temp + a % 10; 
+    } 
+    return a % 10;
+}
+
+int fibonacci_sum_squares_fast(long long n){
+    int last;
+    if(n <= 1){
+        return n;
+    }
+    long long fib[n + 1];
+    fib[0] = 0;
+    fib[1] = 1;
+    long long sum = (fib[0] * fib[0]) + (fib[1] * fib[1]);
+    for(int i = 2; i <= n; i++){
+        fib[i] = fib[i - 1] + fib[i - 2];
+        sum += (fib[i] * fib[i]);
+    }
+    last = get_fibonacci_last_digit_fast(sum);
+    return last;
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+    std::cout << fibonacci_sum_squares_fast(n);
 }
