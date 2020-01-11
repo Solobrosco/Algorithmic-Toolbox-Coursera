4bd8a53fd49069ffe1a547a0b965cbb57e44b9b7
Nikko Solon <nikkosolon24@gmail.com>
2020-01-10 22:44:04 -0800

    Implemented fibonacci_sum()

--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.cpp
@@ -18,8 +18,31 @@ int fibonacci_sum_naive(long long n) {
     return sum % 10;
 }
 
+int get_fibonacci_last_digit_fast(long long n){
+    if(n <= 1){
+        return n;
+    }
+    int a = 0, b = 1;
+    for(int i = 0; i < n - 1; ++i){
+        int temp = b % 10;
+        b = a % 10;
+        a = temp + a % 10;
+    }
+    return a % 10;
+}
+
+int fibonacci_sum_fast(long long n){
+    int x = (n + 2) % 60;
+    int x_end = get_fibonacci_last_digit_fast(x);
+    if(x_end == 0){
+        return 9;
+    }else{
+        return x_end - 1;
+    }
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_naive(n);
+    std::cout << fibonacci_sum_fast(n);
 }
