484be048a50a6b1069af38d5880e47c51468f194
Nikko Solon <nikkosolon24@gmail.com>
2020-01-16 16:40:37 -0800

    Implemented get_optimal_value and added my_Sort in .cpp

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -3,11 +3,40 @@
 
 using std::vector;
 
+void my_Sort(vector<double> &b, vector<int> &v, vector<int> &w){
+  for(int i = 0; i < b.size(); i++){
+    int max = i;
+    for(int j = i+1; j < b.size(); j++){
+      if(b[j] > b[max]){
+        max = j;
+      }
+    }
+    if(max != i){
+      std::iter_swap(b.begin()+i, b.begin()+max);
+      std::iter_swap(v.begin()+i, v.begin()+max);
+      std::iter_swap(w.begin()+i, w.begin()+max);
+    }
+  } 
+}
+
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
 
   // write your code here
-
+  int n = weights.size();
+  vector<double> ben(n);
+  for(int i = 0; i < n; i++){
+    ben[i] = (values[i]/ (double)weights[i]);
+  }
+  my_Sort(ben,values,weights);
+  for(int i = 0; i < n; i++){
+    int seen = std::min(capacity,weights[i]);
+    int res = capacity - seen;
+    if(res >= 0){
+      capacity = res;
+      value += ben[i] * seen;
+    }
+  }
   return value;
 }
 
