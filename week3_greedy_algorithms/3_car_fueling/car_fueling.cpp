c4059ded6597f9238b20a2240e3ff0d2d29d41be
Nikko Solon <nikkosolon24@gmail.com>
2020-01-16 16:41:09 -0800

    Implemented comput_min_refills and added refills in .cpp

--- week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
+++ week3_greedy_algorithms/3_car_fueling/car_fueling.cpp
@@ -6,9 +6,32 @@ using std::cout;
 using std::vector;
 using std::max;
 
+int refills(int dist, int tank, vector<int> &stops, int start, int count){
+    if((start + tank) >= dist){
+        return count;
+    }
+    if(stops.size() == 0){
+        return -1;
+    }
+    int old = start;
+    for(int i = 0; i < stops.size(); i++){
+        if(stops[i] <= (start + tank)){
+            old = stops[i];
+        }else{
+            if(i != 0){
+                stops.erase(stops.begin(), stops.begin() + i);
+            }else{
+                stops.erase(stops.begin());
+            }
+            return refills(dist, tank, stops, old, count + 1);
+        }
+    }
+    return (old + tank) >= dist ? count + 1 : -1;
+}
+
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
     // write your code here
-    return -1;
+    return refills(dist,tank,stops,0,0);
 }
 
 
