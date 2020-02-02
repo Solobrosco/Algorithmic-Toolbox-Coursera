24d5d95364a4720f2fe4a07d973a61d6989f6c27
Nikko Solon <nikkosolon24@gmail.com>
2020-02-01 21:43:41 -0800

    Implemented Dynampic programming solution

--- week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <vector>
 
 int get_change(int m) {
+  int sizeValue = 3;
   //write your code here
-  return m / 4;
+  std::vector<int> change(m + 1);
+  std::vector<int> value = {1, 3, 4};
+  for(size_t i = 1; i <= m; ++i){
+    int min = 1000000000;
+    for(size_t j = 0; j < sizeValue; ++j){
+      if(i >= value[j]){
+        int coin = 1 + change[i - value[j]];
+        if(coin < min){
+          min = coin;
+        }
+      }
+    }
+    change[i] = min;
+  }
+  return change[m];
 }
 
 int main() {
